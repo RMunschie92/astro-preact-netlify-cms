bb138c084622b4b35bf53adc3dbb25fd2d10c4c3
RMunschie92 <ryan@graftonstudio.com>
2022-04-28 18:59:05 -0400

    First use of containers components

---
+++ src/components/containers/Index.jsx
@@ -0,0 +1,40 @@
+//------------------------------------------------------------------------------
+// Imports
+//------------------------------------------------------------------------------
+// Libraries
+
+// CSS
+
+// Components
+import Counter from '../specifics/Counter.jsx';
+
+//------------------------------------------------------------------------------
+// Component
+//------------------------------------------------------------------------------
+const Index = (props) => {
+
+  // Don't render component if content not passed from Astro template
+  if (!props.content) {
+    return null;
+  }
+
+  const { content } = props;
+
+  return (
+    <main>
+      <h2>{content.title}</h2>
+
+      <p>{content.banner_heading}</p>
+
+      <img src={content.banner_image} />
+
+      <article>
+        {[props.children]}
+      </article>
+
+      <Counter />
+    </main>
+  )
+}
+
+export default Index;
